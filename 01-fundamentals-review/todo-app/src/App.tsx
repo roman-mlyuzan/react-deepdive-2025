15a59d91cad96f42e1f3ade4c314f203f2a6354f
Roman M <rmlyuzan@gmail.com>
2025-11-08 15:43:33 +0100

    feat: add search functionality to todo list and enhance input styling

--- 01-fundamentals-review/todo-app/src/App.tsx
+++ 01-fundamentals-review/todo-app/src/App.tsx
@@ -11,7 +11,37 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([])
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
+  const [searchText, setSearchText] = useState<string>('')
 
+  const filteredTodos = todos.filter(todo =>
+    todo.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  const toggleTodo = async (id: number, currentStatus: boolean) => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/' + id, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ completed: !currentStatus })
+      })
+
+      if (!response.ok) throw new Error('Failed to update todo')
+
+      const updatedTodo: Todo = await response.json()
+      setTodos(prevTodos =>
+        prevTodos.map(todo =>
+          todo.id === id ? { ...todo, completed: updatedTodo.completed } : todo
+        )
+      )
+    } catch (err) {
+      console.error('Error:', err);
+      alert('Failed to update todo');
+    }
+  };
+
+  // Initialized only once
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(response => response.json())
@@ -25,6 +55,16 @@ function App() {
       })
   }, []);
 
+  // Runs on every render
+  useEffect(() => {
+    console.log('every render');
+  });
+
+  // Runs when searchText changes
+  useEffect(() => {
+    console.log('Search text changed:', searchText);
+  }, [searchText]);
+
   if (loading) {
     return <div className="loading">Loading...</div>
   }
@@ -36,9 +76,16 @@ function App() {
   return (
     <div className="app-container">
       <h1>My Todos</h1>
+      <input 
+      type="text"
+      value={searchText} 
+      onChange={e => setSearchText(e.target.value)}
+      placeholder="Search todos..."
+      className="search-input"
+      />
       <ul className="todo-list">
-        {todos.map(todo => (
-          <li key={todo.id} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+        {filteredTodos.map(todo => (
+          <li onClick={() => toggleTodo(todo.id, todo.completed)} key={todo.id} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
             <span className="todo-checkbox">{todo.completed ? '✅' : '⭕'}</span>
             <span className="todo-title">{todo.title}</span>
           </li>
